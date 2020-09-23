8f8b7bb31f90616fdfbb80f2cd76186ac096ced4
anst9000 <coltlaboy@yahoo.se>
2020-09-23 18:54:15 +0200

    Project is working can add more Tours

---
+++ src/resources/data/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Come see the beauty and history of lower Manhattan and the Brooklyn Bridge at an easy pace with one of New York’s best tour guides.",
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "After the sun sets on the City of Love, Paris transforms into the City of Lights. Discover the top attractions of the French capital at night on a 2-hour evening walking tour.",
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Discover over 1,000 years of bloody history at the Tower of London, visit the grand Tudor Palace of Henry VIII, one of England's most infamous kings, or marvel at the splendour of the Buckingham Palace state apartments and see the rooms still in use today by Her Majesty and members of the British royal family.",
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "This truly exclusive tour is led by none other than Chef Yoshi Tezuka, the 4th generation sushi master at the 100-year-old Matsunozushi restaurant. After being picked up from your hotel, Chef Yoshi will take you to the famed Tsukiji Fish Market.",
+  },
+];
